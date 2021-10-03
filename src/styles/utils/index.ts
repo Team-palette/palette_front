7301156e2d8d231bb287cc6c292249f0d605404e
An Jihyun <hwiyu25@gmail.com>
2021-10-03 15:25:02 +0900

    edit : utils/index.js에 position 유틸 함수 포함하도록 수정

--- src/styles/utils/index.ts
+++ src/styles/utils/index.ts
@@ -1,3 +1,4 @@
 export { flex } from "./flex";
+export { positionCenter, positionCenterX, positionCenterY } from "./position";
 export * as textStyle from "./textStyle";
 export * as responsive from "./responsive";
