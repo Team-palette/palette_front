1d21f694e06ec7ae6eed78eb54ec55d9d4000a80
An Jihyun <hwiyu25@gmail.com>
2021-10-04 17:31:57 +0900

    style : Header font-size 변경 및 아이콘 추가

--- src/components/Header/HeaderBtns.tsx
+++ src/components/Header/HeaderBtns.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
+import { BiUser } from "react-icons/bi";
 
 import { useRouter } from "../../hooks/useRouter";
 import { getLoginStatus } from "../../redux/module/myInfo/selector";
+import { flex } from "../../styles/utils";
 
 const HeaderBtns = () => {
   const { push } = useRouter();
@@ -12,15 +14,20 @@ const HeaderBtns = () => {
 
   return (
     <BtnBox isLogin={isLogin}>
-      <button type="button" onClick={() => push("/myPage")}>
-        프로필
-      </button>
+      <Btn type="button" onClick={() => push("/myPage")}>
+        <BiUser />
+      </Btn>
     </BtnBox>
   );
 };
 
 const BtnBox = styled.nav<{ isLogin: boolean }>`
   display: ${(props) => !props.isLogin && "none"};
+`;
+
+const Btn = styled.button`
+  ${flex()};
+  font-size: 2rem;
 `;
 
 export default HeaderBtns;
--- src/components/Header/HeaderTitle.tsx
+++ src/components/Header/HeaderTitle.tsx
@@ -7,6 +7,7 @@ import { checkPage } from "../../utils/checkPage";
 
 import { getHomeTitle } from "../../redux/module/myInfo/selector";
 import { positionCenter, positionCenterY } from "../../styles/utils";
+import { sub_1, sub_2 } from "../../styles/utils/textStyle";
 
 const HeaderTitle = () => {
   const { push, pathname } = useRouter();
@@ -24,11 +25,13 @@ const HeaderTitle = () => {
 
 const HomeTitle = styled.h1`
   ${positionCenterY()};
+  ${sub_2}
   left: 0;
 `;
 
 const SiteTitle = styled.h1`
   ${positionCenter()};
+  ${sub_1}
   cursor: pointer;
 `;
 
